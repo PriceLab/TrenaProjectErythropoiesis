b5751565c9b98159be7dedb28c03f72d8906b392
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-07-15 16:49:07 -0700

    old, lingering modifications

--- inst/demos/tbx15-represses-gata2/createFimoSqliteDatabase.R
+++ inst/demos/tbx15-represses-gata2/createFimoSqliteDatabase.R
@@ -1,7 +1,8 @@
 library(TrenaProjectErythropoiesis)
 library(FimoClient)
 library(MotifDb)
-
+library(RSQLite)
+#------------------------------------------------------------------------------------------------------------------------
 if(!exists("tp"))
    tp <- TrenaProjectErythropoiesis()
 if(!exists("tbl.regions")){
@@ -41,7 +42,28 @@ findBindingSites <- function(tbl.regions, threshold=1e-2)
 
 } # findBindingSites
 #------------------------------------------------------------------------------------------------------------------------
-tbl.fimo <- findBindingSites(tbl.regions, threshold=1e-2)
-save(tbl.fimo, file="tbl.fimo.RData")
+run <- function()
+{
+   tbl.regions.gata2.enhancer <- data.frame(chrom="chr3", start=128481976, end=128500133, stringsAsFactors=FALSE)
+   tbl.fimo <- findBindingSites(tbl.regions.gata2.enhancer, threshold=2e-4)
+   save(tbl.fimo, file="tbl.fimo.gata2.enhancer.19k.RData")
+
+   tbl.regions.gata2.enhancer.214k <- data.frame(chrom="chr3", start=128407127, end=128621364, stringsAsFactors=FALSE)
+   tbl.fimo <- findBindingSites(tbl.regions.gata2.enhancer.214k, threshold=2e-4)
+     # dim(tbl.fimo)  # [1] 302522     10
+     # length(grep("TBX15", tbl.fimo$tf)) 2663   10
+   save(tbl.fimo, file="tbl.regions.gata2.enhancer.214k.RData")
+   db <- dbConnect(SQLite(), "fimoResults-2e-4-chr3-128074944-128620958-214k.sqlite")
+   dbWriteTable(db, name="fimoBindingSites", value=tbl.fimo, overwrite=TRUE)
+   dbDisconnect(db)
+
+     # try it out
+   db <- dbConnect(SQLite(), "fimoResults-2e-4-chr3-128074944-128620958-214k.sqlite")
+   tbl.atac.538bp <- dbGetQuery(db, "select * from fimoBindingSites where chrom='chr3' and start >= 128497516 and end <= 128498053")
+   dim(tbl.atac.538bp)  # 1748 x 10
+
+
+} # run
+#------------------------------------------------------------------------------------------------------------------------
 
 
