e78cc745bd2d5c47481ec92b51beeed0a5c9ab7b
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-07-23 12:22:44 -0700

    draft for r2d3

---
+++ prep/import/srm-rna-averaged-final-for-paper/readData.R
@@ -0,0 +1,20 @@
+mtx.rna <- as.matrix(read.table("rna.txt", sep="\t", as.is=TRUE, header=TRUE, row.names=1, nrow=-1))
+mtx.srm <- as.matrix(read.table("srm.txt", sep="\t", as.is=TRUE, header=TRUE, row.names=1, nrow=-1))
+dim(mtx.rna)
+dim(mtx.srm)
+
+setdiff(rownames(mtx.rna), rownames(mtx.srm))
+setdiff(rownames(mtx.srm), rownames(mtx.rna))
+
+common.names <- sort(intersect(rownames(mtx.rna), rownames(mtx.srm)))
+length(common.names)
+
+mtx.rna <- mtx.rna[common.names,]
+mtx.srm <- mtx.srm[common.names,]
+save(mtx.rna, mtx.srm, file="srm.rna.averaged.clean.RData")
+fivenum(mtx.rna)
+fivenum(mtx.srm)
+boxplot(asinh(as.numeric(mtx.rna)), asinh(as.numeric(mtx.srm)))
+
+
+
